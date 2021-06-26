ee84b10138526554952b7af76878c09dc2041b13
ignaciocervino <ignaciocervino@gmail.com>
2021-06-26 19:20:42 -0300

    #05 Validating the message

--- js/app.js
+++ js/app.js
@@ -13,5 +13,23 @@ function eventListeners(){
 /** FUNCTIONS */
 function agregarTweet(e){
     e.preventDefault();
-    console.log('Agregando Tweet');
+    //textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+    if(tweet === ''){
+        mostrarError('Un mensaje no puede ir vacio');
+        return; //Evita que se siga ejecutando la funcion
+    }
+    console.log(tweet);
+}
+
+function mostrarError(mensaje){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('error');
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    setTimeout(()=>{
+        mensajeError.remove();
+    },3000);
 }
